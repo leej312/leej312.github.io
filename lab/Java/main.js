7e2a909cf158b0208126d91fc057838d44caaaaa
leej312 <leej312@newschool.edu>
2018-04-17 03:52:10 -0400

    masterpiece

---
+++ lab/Java/main.js
@@ -0,0 +1,63 @@
+
+var myWeekend ='crossfit';
+
+console.log(6 * 11); 
+console.log(61 + 5);	
+console.log('6' + '6');	
+console.log(660 / 10);	
+console.log(70 - 4);
+console.log(6 + 60);	
+
+var students = [
+  'Kristen',
+  'Evelin',
+  'Vincent',
+  'Yangchen',
+  'Esra',
+  'Shuting',
+  'Yaren',
+  'Prithi',
+  'Soyeon',
+  'Seowon',
+  'Jahyun',
+  'Emily',
+  'Khe',
+  'Vincent',
+  'Emil']
+
+var group1 = students[0,1,4,7,11,13]
+var group2 = students[5]
+var group3 = students[12]
+var group4 = students[2]
+var group5 = students[8,9,10]
+var group6 = students[3,6]
+
+
+var studentsMissing ['no one is missing']
+console.log(0);
+
+for ( var i = 0; i < 10; i++ ) {
+  console.log('The index is', i);
+}
+
+var students = [
+  'Kristen',
+  'Evelin',
+  'Vincent',
+  'Yangchen',
+  'Esra',
+  'Shuting',
+  'Yaren',
+  'Prithi',
+  'Soyeon',
+  'Seowon',
+  'Jahyun',
+  'Emily',
+  'Khe',
+  'Vincent',
+  'Emil'
+];
+
+for ( var i = 0; i < students.length; i++ ) {
+  console.log( students[i] + ' is in index ' + i );
+}
